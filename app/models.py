8b12107197ef8bb86dccd3cc5144a7a5a4205e53
kiyimba1 <edwineddie54@gmail.com>
2020-01-24 17:57:05 +0300

    fixed models

--- app/models.py
+++ app/models.py
@@ -72,10 +72,8 @@ class Role(db.Model):
 
 class Follow(db.Model):
     __tablename__ = 'follows'
-    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'),
-                            primary_key=True)
-    followed_id = db.Column(db.Integer, db.ForeignKey('users.id'),
-                            primary_key=True)
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'),  primary_key=True)
+    followed_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
 
---
+++ tests/test_post_model.py
@@ -0,0 +1,3 @@
+import unittest
+from app import create_app, db
+from app.models import Post, Comment
